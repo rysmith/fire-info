8dbde3e1db4f75790983dff1d79f1a2fba84979c
Ryan Smith <ryansmith05@gmail.com>
2020-09-13 19:27:05 -0700

    break up widget builders a bit

--- scripts/aqiWidgets.js
+++ scripts/aqiWidgets.js
@@ -1,27 +1,6 @@
 var aqiWidgets = (function() {
     var containerId = 'aqi-widgets'
-
-    function widgetRow(row, widgetContainer) {
-        var rowElement = domUtility.buildNode('div', '', [{ key: 'class', value: 'row' }]);
-
-        widgetContainer.appendChild(rowElement);
-
-        row.forEach(function(widget) { widgetRowItem(widget, rowElement) });
-    }
-
-    function widgetRowItem(item, rowElement) {
-        var rowItemId = 'purpleAirWidget-' + item.id
-        var rowItem = domUtility.buildNode('div', '', [
-            { key: 'class', value: 'row-item' },
-            { key: 'id', value: rowItemId }
-        ]);
-        var h2 = domUtility.buildNode('h2', item.title)
-
-        h2.appendChild(button(item.url_position))
-        domUtility.appendChildren(rowItem, h2, widget(item.id))
-
-        rowElement.appendChild(rowItem);
-    }
+    var container = document.getElementById(containerId);
 
     function button(url_position) {
         var icon = domUtility.buildIcon('far fa-map');
@@ -36,7 +15,9 @@ var aqiWidgets = (function() {
     function buttonClickHandler(index) {
         var widgetElements = document.getElementsByClassName("legend-tooltip deviceIcon nb");
 
-        return function openMap() { window.open(widgetElements[index].firstChild.href) }
+        return function openMap() {
+            window.open(widgetElements[index].firstChild.href)
+        }
     }
 
     function widget(widget_id) {
@@ -63,18 +44,46 @@ var aqiWidgets = (function() {
         widget.appendChild(wrapper);
     }
 
+    function widgetRow(container) {
+        return function widgetRowItem(row) {
+            var rowElement = domUtility.buildNode('div', '', [{ key: 'class', value: 'row' }]);
+
+            container.appendChild(rowElement);
+
+            row.forEach(function(widget) { appendRowItem(widget, rowElement) });
+        }
+    }
+
+    function appendRowItem(item, rowElement) {
+        var rowItemId = 'purpleAirWidget-' + item.id
+        var rowItem = domUtility.buildNode('div', '', [
+            { key: 'class', value: 'row-item' },
+            { key: 'id', value: rowItemId }
+        ]);
+        var h2 = domUtility.buildNode('h2', item.title)
+
+        h2.appendChild(button(item.url_position))
+        domUtility.appendChildren(rowItem, h2, widget(item.id))
+
+        rowElement.appendChild(rowItem);
+    }
+
+    function h1(container) {
+        var titleIcon = domUtility.buildIcon('fas fa-wind');
+        var titleText = document.createTextNode(' PM 2.5');
+        var header = domUtility.buildNode('h1');
+
+        domUtility.appendChildren(header, titleIcon, titleText)
+        container.appendChild(header)
+    }
+
+
     return {
         build: function(sensors) {
-            var widgetContainer = document.getElementById(containerId);
+            var widgetRowItem = widgetRow(container)
 
-            var titleIcon = domUtility.buildIcon('fas fa-wind');
-            var titleText = document.createTextNode(' PM 2.5');
-            var h1 = domUtility.buildNode('h1');
-
-            domUtility.appendChildren(h1, titleIcon, titleText)
-            widgetContainer.appendChild(h1)
-
-            sensors.forEach(function(sensor) { widgetRow(sensor, widgetContainer) });
+            h1(container)
+            sensors.forEach(function(sensor) { widgetRowItem(sensor) });
             spinners();
         },
     }

5faf7b87c4f3be258b2bdc1ce840c7f9fd49af4d
Ryan Smith <ryansmith05@gmail.com>
2020-09-16 20:04:08 -0700

    consolidate functions in fire module

--- scripts/creekFire.js
+++ scripts/creekFire.js
@@ -1,5 +1,6 @@
 var creekFire = (function() {
     var containerId = 'creek-fire'
+    var container = document.getElementById(containerId);
 
     function evacInfo(wrapper_style, icon_style, title, text) {
         var style = wrapper_style + ' evac-info'
@@ -16,33 +17,6 @@ var creekFire = (function() {
         return wrapper;
     }
 
-    function evacWarnings1() {
-        return evacInfo(
-            'warning',
-            'fas fa-info-circle',
-            'Evacuation Warnings',
-            'Note that some evacuation warnings have been lifted for areas of Oakhurst and Corsegold just East of Highway 41.  See incident map for more details.'
-        );
-    }
-
-    function evacWarnings2() {
-        return evacInfo(
-            'warning',
-            'fas fa-info-circle',
-            'Evacuation Warnings',
-            'Oakhurst, O’Neals, Walker Grade, Chukchansi, Indian Lakes, and the portion of Coarsegold east of Highway 41.  '
-        );
-    }
-
-    function buildEvacOrders() {
-        return evacInfo(
-            'danger',
-            'fas fa-exclamation-triangle',
-            'Evacuation Orders',
-            'Bass Lake Basin, including Wishon, Bass Lake Annex, and Manzanita; the town of North Fork and surrounding area.'
-        );
-    }
-
     function evacuationMap() {
         return domUtility.buildNode('iframe', '', [
             { key: 'title', value: 'Evacuation Overlay' },
@@ -52,29 +26,56 @@ var creekFire = (function() {
         ]);
     }
 
+    function header(text, icon) {
+        var h1 = domUtility.buildNode('h1');
+        var fireIcon = domUtility.buildIcon(icon);
+        var sectionTitle = document.createTextNode(' ' + text);
+        domUtility.appendChildren(h1, fireIcon, sectionTitle);
+
+        return h1
+    }
+
+    function headerLink(text, url) {
+        var subheader = domUtility.buildNode('h1');
+        var incidentLink = domUtility.buildNode('a', text, [
+            { key: 'href', value: url },
+            { key: 'target', value: '_blank' }
+        ])
+
+        domUtility.appendChildren(subheader, domUtility.buildIcon('fas fa-external-link-alt'), document.createTextNode(' '), incidentLink);
+
+        return subheader
+    }
+
+    function build() {
+        domUtility.appendChildren(container,
+            header('Creek Fire', 'fas fa-fire-alt'),
+            headerLink('Evacuation Map', 'https://nifc.maps.arcgis.com/apps/webappviewer/index.html?id=28ce52cb86bf49dea0adef5a78b3c1b5'),
+            headerLink('Incident Site', 'https://inciweb.nwcg.gov/incident/7147/'),
+            evacInfo(
+                'danger',
+                'fas fa-exclamation-triangle',
+                'Evacuation Orders',
+                'Bass Lake Basin, including Wishon, Bass Lake Annex, and Manzanita; the town of North Fork and surrounding area.'
+            ),
+            evacInfo(
+                'warning',
+                'fas fa-info-circle',
+                'Evacuation Warning Update',
+                'Note that some evacuation warnings have been lifted for areas of Oakhurst and Corsegold just East of Highway 41.  See evacuation map for more details.'
+            ),
+            evacInfo(
+                'warning',
+                'fas fa-info-circle',
+                'Evacuation Warnings',
+                'Oakhurst, O’Neals, Walker Grade, Chukchansi, Indian Lakes, and the portion of Coarsegold east of Highway 41.'
+            ),
+            header('Embeded Evacuation Map', 'far fa-map'),
+            evacuationMap()
+        );
+    }
+
     return {
-        build: function() {
-            var creekFireContainer = document.getElementById(containerId);
-            var fireIcon = domUtility.buildIcon('fas fa-fire-alt');
-            var sectionTitle = document.createTextNode(' Creek Fire');
-            var incidentLink = domUtility.buildNode('a', 'Incident Site', [
-                { key: 'href', value: 'https://inciweb.nwcg.gov/incident/7147/' },
-                { key: 'target', value: '_blank' }
-            ])
-
-            var h1 = domUtility.buildNode('h1');
-            var subheader = domUtility.buildNode('h1');
-
-            domUtility.appendChildren(h1, fireIcon, sectionTitle);
-            domUtility.appendChildren(subheader, domUtility.buildIcon('fas fa-external-link-alt'), document.createTextNode(' '), incidentLink);
-            domUtility.appendChildren(creekFireContainer,
-                h1,
-                buildEvacOrders(),
-                evacWarnings1(),
-                evacWarnings2(),
-                subheader,
-                evacuationMap()
-            );
-        }
+        build: build
     }
 })();
